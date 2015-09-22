0a516ef3c17fe3db9059ff1f7a75ea11a7b2d126
thanhgtoken <thanh.tran@gtoken.com>
2015-09-22 15:40:24 +0700

    create project

---
+++ SSSTest/SSSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSSTest
+//
+//  Created by Thanh Tran on 9/22/15.
+//  Copyright (c) 2015 thanhtran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
